7b3f2ec4c990c37efef297e2be995d63163b204e
manosanag <manosanag@ics.forth.gr>
2022-12-15 01:58:52 +0200

    [fix] Script additions. Fixed options.yml path to absolute.

--- compile.sh
+++ compile.sh
@@ -1,6 +1,13 @@
-rm -rf build
+if [ $1 -e "-c" ]
+then
+	rm -rf build
+fi
 mkdir -p build
 cd build
-cmake3 .. -DCMAKE_BUILD_TYPE="Release" .
+if [ $1 -e "-c" ]
+then
+	cmake3 .. -DCMAKE_BUILD_TYPE="Release" .
+fi
 make
-
+rm -rf /opt/manosanag/teraheap-opensrc/allocator/ummap-io/libparallax.so
+cp -r lib/libparallax.so /opt/manosanag/teraheap-opensrc/allocator/ummap-io/
